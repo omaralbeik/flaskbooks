6f133d6014d969d603601b20bbf62e262267f768
Omar Albeik <omaralbeik@gmail.com>
2017-06-22 18:00:44 +0300

    Added edit and new endpoints for books

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -23,32 +23,155 @@ session = DBSession()
 @app.route('/')
 @app.route('/books/')
 def books():
-    books = session.query(Book).all()
-    return render_template('books.html', books=books)
+    return render_books()
 
 # show book info
 @app.route('/book/<int:book_id>')
 def book(book_id):
-    book = session.query(Book).filter_by(id=book_id).one()
-    return render_template('book.html', book=book)
+    return render_book(book_id)
 
 # show genre
 @app.route('/genre/<int:genre_id>/')
 def genre(genre_id):
-    genre = session.query(Genre).filter_by(id=genre_id).one()
-    books = session.query(Book).filter_by(genre_id=genre_id)
-    return render_template('genre.html', genre=genre, books=books)
-
+    return render_genre(genre_id)
 
 # show user
 @app.route('/user/<int:user_id>/')
 def user(user_id):
+    return render_user(user_id)
+
+# Add a new book
+@app.route('/books/new/', methods=['GET', 'POST'])
+def newBook():
+    if request.method == 'GET':
+        return render_new_book()
+
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        genre_id = request.form['genre_id']
+        cover_image_url = request.form['cover_image_url']
+        pages_count = request.form['pages_count']
+        author_name = request.form['author_name']
+        year = request.form['year']
+        price = request.form['price']
+
+        if not name:
+            flash("Please enter book name")
+            return render_new_book()
+
+        if not genre_id:
+            flash("Please select a genre")
+            return render_new_book()
+
+        book = Book(name=name, genre_id=genre_id, user_id=1)
+
+        book.description = description if description else None
+        book.cover_image_url = cover_image_url if cover_image_url else None
+        book.pages_count = pages_count if pages_count else None
+        book.author_name = author_name if author_name else None
+        book.year = year if year else None
+        book.price = price if price else None
+
+        session.add(book)
+        session.commit()
+
+        return redicrect_books()
+
+
+# Edit book
+@app.route('/book/<int:book_id>/edit/', methods=['GET', 'POST'])
+def editBook(book_id):
+    if request.method == 'GET':
+        return render_edit_book(book_id)
+
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        genre_id = request.form['genre_id']
+        cover_image_url = request.form['cover_image_url']
+        pages_count = request.form['pages_count']
+        author_name = request.form['author_name']
+        year = request.form['year']
+        price = request.form['price']
+
+        if not name:
+            flash("Please enter book name")
+            return render_new_book()
+
+        if not genre_id:
+            flash("Please select a genre")
+            return render_new_book()
+
+        book = session.query(Book).filter_by(id=book_id).one()
+
+        book.description = description if description else None
+        book.cover_image_url = cover_image_url if cover_image_url else None
+        book.pages_count = pages_count if pages_count else None
+        book.author_name = author_name if author_name else None
+        book.year = year if year else None
+        book.price = price if price else None
+
+        session.add(book)
+        session.commit()
+
+        return redicrect_books()
+
+
+
+########################################
+###        Render Functions          ###
+########################################
+
+def render_books():
+    books = session.query(Book).all()
+    return render_template('books.html', books=books)
+
+def render_book(book_id):
+    book = session.query(Book).filter_by(id=book_id).one()
+    return render_template('book.html', book=book)
+
+def render_new_book():
+    genres = session.query(Genre).all()
+    return render_template('newbook.html', genres=genres)
+
+def render_edit_book(book_id):
+    book = session.query(Book).filter_by(id=book_id).one()
+    genres = session.query(Genre).all()
+    return render_template('editbook.html', book=book, genres=genres)
+
+def render_genre(genre_id):
+    genre = session.query(Genre).filter_by(id=genre_id).one()
+    books = session.query(Book).filter_by(genre_id=genre_id)
+    return render_template('genre.html', genre=genre, books=books)
+
+def render_user(user_id):
     user = session.query(User).filter_by(id=user_id).one()
     genres = session.query(Genre).filter_by(user_id=user_id).all()
     books = session.query(Book).filter_by(user_id=user_id).all()
     return render_template('user.html', user=user, genres=genres, books=books)
 
 
+########################################
+###       Redicrect Functions        ###
+########################################
+def redicrect_books():
+    return redirect('books')
+
+def redicrect_book(book_id):
+        return redirect('book/%s', book_id)
+
+def redicrect_new_book():
+    return redicrect('newbook')
+
+def redicrect_edit_book(book_id):
+    return redirect('editbook/%s', book_id)
+
+def redicrect_genre(genre_id):
+    return redirect('genre/%s', genre_id)
+
+def redicrect_user(user_id):
+    return redirect('user/%s', user_id)
 
 
 ########################################
@@ -114,5 +237,6 @@ def bookJSON(book_id):
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
--- bookstore/lots_of_books.py
+++ bookstore/lots_of_books.py
@@ -13,27 +13,30 @@ session.query(Genre).delete()
 session.query(Book).delete()
 session.commit()
 
-user1 = User(id=1, name="Omar Albeik", email="omaralbeik@gmail.com")
-user2 = User(id=2, name="Mert Akengin", email="mert.akengin@gmail.com")
-user3 = User(id=3, name="John Applesead", email="john.applesead@apple.com")
+user1 = User(name="Omar Albeik", email="omaralbeik@gmail.com")
+user2 = User(name="Mert Akengin", email="mert.akengin@gmail.com")
+user3 = User(name="John Applesead", email="john.applesead@apple.com")
 users = [user1, user2, user3]
 for u in users:
     session.add(u)
 session.commit()
 
-genre1 = Genre(id=1, name="Non Fiction", user_id=1)
-genre2 = Genre(id=2, name="Fiction", user_id=2)
+genre1 = Genre(name="Non Fiction", user_id=1)
+genre2 = Genre(name="Fiction", user_id=2)
 genres = [genre1, genre2]
 for g in genres:
     session.add(g)
 session.commit()
 
-book1 = Book(id=1, name='Silent Spring', description="Silent Spring is an environmental science book by Rachel Carson. The book was published on 27 September 1962 and it documented the detrimental effects on the environment of the indiscriminate use of pesticides.", author_name="Rachel Carson", pages_count = 264, year=2004, price=8.95, genre_id=1, user_id=1)
+book1 = Book(name='Silent Spring', description="Silent Spring is an environmental science book by Rachel Carson. The book was published on 27 September 1962 and it documented the detrimental effects on the environment of the indiscriminate use of pesticides.", author_name="Rachel Carson", pages_count = 264, year=2004, price=8.95, genre_id=1, user_id=1, cover_image_url="https://images.gr-assets.com/books/1442353674l/27333.jpg")
 
-book2 = Book(id=2, name="The Handmaid's Tale", description="The Handmaid's Tale is set in the Republic of Gilead, a theocratic military dictatorship formed within the borders of what was formerly the United States of America. ... Offred describes the structure of Gilead's society, including the several different classes of women and their circumscribed lives in the new theocracy.", author_name="Margaret Atwood", year=1985, price=9.81, genre_id=2, user_id=2)
+book2 = Book(name="The Handmaid's Tale", description="The Handmaid's Tale is set in the Republic of Gilead, a theocratic military dictatorship formed within the borders of what was formerly the United States of America. ... Offred describes the structure of Gilead's society, including the several different classes of women and their circumscribed lives in the new theocracy.", author_name="Margaret Atwood", year=1985, price=9.81, genre_id=2, user_id=2, cover_image_url="https://images.gr-assets.com/books/1498057733l/38447.jpg")
 
-book3 = Book(id=3, name="Ender's Game", description="Ender's Game is a 1985 military science fiction novel by American author Orson Scott Card. Set in Earth's future, the novel presents an imperiled mankind after two conflicts with the \"buggers\", an insectoid alien species.", pages_count = 420, author_name="Orson Scott Card", year=1985, price=12.81, genre_id=2, user_id=3)
-books = [book1, book2, book3]
+book3 = Book(name="Ender's Game", description="Ender's Game is a 1985 military science fiction novel by American author Orson Scott Card. Set in Earth's future, the novel presents an imperiled mankind after two conflicts with the \"buggers\", an insectoid alien species.", pages_count = 420, author_name="Orson Scott Card", year=1985, price=12.81, genre_id=2, user_id=3, cover_image_url="https://images.gr-assets.com/books/1408303130l/375802.jpg")
+
+book4 = Book(name="Bossypants", description="Before Liz Lemon, before \"Weekend Update,\" before \"Sarah Palin,\" Tina Fey was just a young girl with a dream: a recurring stress dream that she was being chased through a local airport by her middle-school gym teacher. She also had a dream that one day she would be a comedian on TV.", pages_count = 283, author_name="Tina Fey", year=2011, genre_id=1, user_id=3, cover_image_url="https://images.gr-assets.com/books/1481509554l/9418327.jpg")
+
+books = [book1, book2, book3, book4]
 for b in books:
     session.add(b)
 session.commit()
