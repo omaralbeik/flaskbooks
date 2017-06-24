0a4ec276d59724f13a59e157057b366c428d51a2
Omar Albeik <omaralbeik@gmail.com>
2017-06-24 04:15:31 +0300

    Update bookstore.py

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -402,4 +402,4 @@ def userJSON(user_id):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
