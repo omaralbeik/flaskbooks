8d11b020b7765c57eed2360a7b7547ced171c515
Omar Albeik <omaralbeik@gmail.com>
2017-07-06 01:53:57 +0300

    Update lots_of_books.py

--- flask_books/lots_of_books.py
+++ flask_books/lots_of_books.py
@@ -14,7 +14,7 @@ session.query(Book).delete()
 session.commit()
 
 users = [
-    User(name="Omar Albeik", email="omaralbeik@gmail.com", bio="I\’m a passionate software developer, AI enthusiast and lifelong learner!"),
+    User(name="Omar Albeik", email="omaralbeik@gmail.com", bio="A passionate software developer, AI enthusiast and lifelong learner."),
     User(name="Jane Doe", email="jane.doe@gmail.com"),
     User(name="John Applesead", email="john.applesead@apple.com")
 ]
